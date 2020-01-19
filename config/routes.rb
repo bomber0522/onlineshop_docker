31344d5a5a3755868df559f4c1db53a77f2c67d9
Takaki <takaki55730317@gmail.com>
2020-01-19 19:36:19 +0900

    Modified members_controller.rb - Created members/edit.html.erb - Created members/show.html.erb - Modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
   get "contact" => "top#contact"
-  get "/signup", to: "members#new"
+  get "/signup",  to: "members#new"
   post "/signup", to: "members#create"
+  get "/login",   to: "sessions#new"
+  post "/login",  to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   resources :member
 end
