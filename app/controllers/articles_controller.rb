2ad38bd62326c25614af951be2d6edd60b5479a6
Takaki <takaki55730317@gmail.com>
2020-01-21 18:50:35 +0900

    Modified Gemfile - Created app/controllers/articles_controller.rb - Modified app/helpers/application_helper.rb - Deleted app/helpers/top_helper.rb - Created app/models/article.rb - Modified config/routes.rb - Created User model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.visible.order(released_at: :desc)
+    @atricles = @articles.open_to_the_public unless current_member
+    @articles = @articles.paginate(page: params[:page], per_page: 4)
+  end
+
+  def show
+    articles = Article.visible
+    articles = articles.open_to_the_public unless current_member
+    @article = articles.find(params[:id])
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,11 @@ module ApplicationHelper
   
   def menu_link_to(text, path, options = {})
     content_tag :li do
+      condition = options[:method]
 
+      link_to_if(condition, text, path, options) do
+        content_tag(:span, text)
+      end
     end
   end
 end
