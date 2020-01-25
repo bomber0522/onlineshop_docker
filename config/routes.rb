8cb7923b61a302cf59b997acb723a6987e68f359
Takaki <takaki55730317@gmail.com>
2020-01-25 12:26:25 +0900

    Create app/views/admin/members/index.html.erb - Modified config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,10 @@ Rails.application.routes.draw do
       patch :move_higher, :move_lower, on: :member
     end
   end
+
+  namespace :admin do
+    root to: "top#index"
+    resources :members
+    resources :ariticles
+  end
 end
